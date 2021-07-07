e03f6b671cdf8f0b7f694c8e07a883a83c9540c3
Pablo Salazar <pablosalazar66@hotmail.com>
2021-07-07 00:05:05 -0500

    Improve countries CRUD

---
+++ OnSale.Commom/Entities/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OnSale.Commom.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [MaxLength(50, ErrorMessage = "The filed {0} must contain less than {1} charecteres.")]
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ OnSale.Web/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnSale.Commom.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnSale.Web.Data
+{
+    public class DataContext : DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+         public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>()
+            .HasIndex(t => t.Name)
+            .IsUnique();
+
+        }
+    }
+}
